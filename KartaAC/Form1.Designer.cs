f734dc119f9de43040685465e34e9baa1ac9cdb4
Gaba54 <72696284+Gaba54@users.noreply.github.com>
2022-04-03 16:01:38 +0200

    AddingScrollBar

--- KartaAC/Form1.Designer.cs
+++ KartaAC/Form1.Designer.cs
@@ -33,19 +33,31 @@
             System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBoxMin = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBoxMinX = new System.Windows.Forms.TextBox();
+            this.textBoxMaxX = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBoxMax = new System.Windows.Forms.Label();
+            this.textMax = new System.Windows.Forms.Label();
             this.OpenFile = new System.Windows.Forms.Button();
-            this.ChanegRange = new System.Windows.Forms.Button();
+            this.ChanegRangeX = new System.Windows.Forms.Button();
             this.GenerateChart = new System.Windows.Forms.Button();
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.tabPage1 = new System.Windows.Forms.Panel();
+            this.ChanegRangeY = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxMaxY = new System.Windows.Forms.TextBox();
+            this.textBoxMinY = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.PlotY = new System.Windows.Forms.Button();
+            this.PlotX = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.ChartScroll = new System.Windows.Forms.HScrollBar();
+            this.ExportToPDF = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
             this.SuspendLayout();
             // 
@@ -64,75 +76,76 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(743, 38);
             this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(1469, 767);
+            this.label2.Location = new System.Drawing.Point(1013, 873);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(313, 32);
+            this.label2.Size = new System.Drawing.Size(209, 32);
             this.label2.TabIndex = 4;
-            this.label2.Text = "Zmien zakres amplitudy";
+            this.label2.Text = "Zmien zakres X";
             // 
-            // textBoxMin
+            // textBoxMinX
             // 
-            this.textBoxMin.Location = new System.Drawing.Point(1279, 863);
-            this.textBoxMin.Name = "textBoxMin";
-            this.textBoxMin.Size = new System.Drawing.Size(100, 38);
-            this.textBoxMin.TabIndex = 5;
+            this.textBoxMinX.Location = new System.Drawing.Point(1639, 903);
+            this.textBoxMinX.Name = "textBoxMinX";
+            this.textBoxMinX.Size = new System.Drawing.Size(162, 38);
+            this.textBoxMinX.TabIndex = 5;
             // 
-            // textBox3
+            // textBoxMaxX
             // 
-            this.textBox3.Location = new System.Drawing.Point(1082, 872);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 38);
-            this.textBox3.TabIndex = 6;
+            this.textBoxMaxX.Location = new System.Drawing.Point(1426, 903);
+            this.textBoxMaxX.Name = "textBoxMaxX";
+            this.textBoxMaxX.Size = new System.Drawing.Size(162, 38);
+            this.textBoxMaxX.TabIndex = 6;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(1282, 819);
+            this.label3.Location = new System.Drawing.Point(1642, 859);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(68, 32);
+            this.label3.Size = new System.Drawing.Size(94, 32);
             this.label3.TabIndex = 7;
-            this.label3.Text = "Min:";
+            this.label3.Text = "Min X:";
             // 
-            // textBoxMax
+            // textMax
             // 
-            this.textBoxMax.AutoSize = true;
-            this.textBoxMax.Location = new System.Drawing.Point(1083, 828);
-            this.textBoxMax.Name = "textBoxMax";
-            this.textBoxMax.Size = new System.Drawing.Size(75, 32);
-            this.textBoxMax.TabIndex = 8;
-            this.textBoxMax.Text = "Max:";
+            this.textMax.AutoSize = true;
+            this.textMax.Location = new System.Drawing.Point(1427, 859);
+            this.textMax.Name = "textMax";
+            this.textMax.Size = new System.Drawing.Size(101, 32);
+            this.textMax.TabIndex = 8;
+            this.textMax.Text = "Max X:";
             // 
             // OpenFile
             // 
-            this.OpenFile.Location = new System.Drawing.Point(949, 19);
+            this.OpenFile.Location = new System.Drawing.Point(1599, 19);
             this.OpenFile.Name = "OpenFile";
-            this.OpenFile.Size = new System.Drawing.Size(238, 57);
+            this.OpenFile.Size = new System.Drawing.Size(258, 61);
             this.OpenFile.TabIndex = 12;
-            this.OpenFile.Text = "Open file";
+            this.OpenFile.Text = "Otwórz plik";
             this.OpenFile.UseVisualStyleBackColor = true;
             this.OpenFile.Click += new System.EventHandler(this.OpenFile_Click);
             // 
-            // ChanegRange
+            // ChanegRangeX
             // 
-            this.ChanegRange.Location = new System.Drawing.Point(1501, 863);
-            this.ChanegRange.Name = "ChanegRange";
-            this.ChanegRange.Size = new System.Drawing.Size(254, 52);
-            this.ChanegRange.TabIndex = 13;
-            this.ChanegRange.Text = "Zmien zakres";
-            this.ChanegRange.UseVisualStyleBackColor = true;
-            this.ChanegRange.Click += new System.EventHandler(this.ChanegRange_Click);
+            this.ChanegRangeX.Location = new System.Drawing.Point(1923, 873);
+            this.ChanegRangeX.Name = "ChanegRangeX";
+            this.ChanegRangeX.Size = new System.Drawing.Size(254, 52);
+            this.ChanegRangeX.TabIndex = 13;
+            this.ChanegRangeX.Text = "Zmien zakres";
+            this.ChanegRangeX.UseVisualStyleBackColor = true;
+            this.ChanegRangeX.Click += new System.EventHandler(this.ChanegRangeX_Click);
             // 
             // GenerateChart
             // 
-            this.GenerateChart.Location = new System.Drawing.Point(361, 1122);
+            this.GenerateChart.Location = new System.Drawing.Point(294, 1128);
             this.GenerateChart.Name = "GenerateChart";
-            this.GenerateChart.Size = new System.Drawing.Size(275, 78);
+            this.GenerateChart.Size = new System.Drawing.Size(347, 78);
             this.GenerateChart.TabIndex = 14;
-            this.GenerateChart.Text = "Generuj";
+            this.GenerateChart.Text = "Generuj wykres X i Y ";
             this.GenerateChart.UseVisualStyleBackColor = true;
             this.GenerateChart.Click += new System.EventHandler(this.GenerateChart_Click);
             // 
@@ -148,7 +161,86 @@
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Size = new System.Drawing.Size(910, 962);
             this.tabPage1.TabIndex = 16;
-            this.tabPage1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // ChanegRangeY
+            // 
+            this.ChanegRangeY.Location = new System.Drawing.Point(1923, 982);
+            this.ChanegRangeY.Name = "ChanegRangeY";
+            this.ChanegRangeY.Size = new System.Drawing.Size(254, 52);
+            this.ChanegRangeY.TabIndex = 23;
+            this.ChanegRangeY.Text = "Zmien zakres";
+            this.ChanegRangeY.UseVisualStyleBackColor = true;
+            this.ChanegRangeY.Click += new System.EventHandler(this.ChanegRangeY_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1427, 968);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(101, 32);
+            this.label4.TabIndex = 22;
+            this.label4.Text = "Max Y:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(1642, 968);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 32);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "Min Y:";
+            // 
+            // textBoxMaxY
+            // 
+            this.textBoxMaxY.Location = new System.Drawing.Point(1426, 1012);
+            this.textBoxMaxY.Name = "textBoxMaxY";
+            this.textBoxMaxY.Size = new System.Drawing.Size(162, 38);
+            this.textBoxMaxY.TabIndex = 20;
+            // 
+            // textBoxMinY
+            // 
+            this.textBoxMinY.Location = new System.Drawing.Point(1639, 1012);
+            this.textBoxMinY.Name = "textBoxMinY";
+            this.textBoxMinY.Size = new System.Drawing.Size(162, 38);
+            this.textBoxMinY.TabIndex = 19;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(1013, 982);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(209, 32);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Zmien zakres Y";
+            // 
+            // PlotY
+            // 
+            this.PlotY.Location = new System.Drawing.Point(1687, 1084);
+            this.PlotY.Name = "PlotY";
+            this.PlotY.Size = new System.Drawing.Size(286, 67);
+            this.PlotY.TabIndex = 24;
+            this.PlotY.Text = "Wykres Y";
+            this.PlotY.UseVisualStyleBackColor = true;
+            this.PlotY.Click += new System.EventHandler(this.PlotY_Click);
+            // 
+            // PlotX
+            // 
+            this.PlotX.Location = new System.Drawing.Point(1151, 1084);
+            this.PlotX.Name = "PlotX";
+            this.PlotX.Size = new System.Drawing.Size(286, 67);
+            this.PlotX.TabIndex = 25;
+            this.PlotX.Text = "Wykres X";
+            this.PlotX.UseVisualStyleBackColor = true;
+            this.PlotX.Click += new System.EventHandler(this.PlotX_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(998, 28);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(554, 39);
+            this.comboBox1.TabIndex = 26;
+            this.comboBox1.SelectedValueChanged += new System.EventHandler(this.comboBox1_SelectedValueChanged);
             // 
             // chart2
             // 
@@ -156,7 +248,7 @@
             this.chart2.ChartAreas.Add(chartArea1);
             legend1.Name = "Legend1";
             this.chart2.Legends.Add(legend1);
-            this.chart2.Location = new System.Drawing.Point(998, 111);
+            this.chart2.Location = new System.Drawing.Point(1007, 111);
             this.chart2.Name = "chart2";
             series1.ChartArea = "ChartArea1";
             series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
@@ -173,21 +265,56 @@
             this.chart2.Size = new System.Drawing.Size(1198, 623);
             this.chart2.TabIndex = 17;
             this.chart2.Text = "chart2";
+            title1.Alignment = System.Drawing.ContentAlignment.TopLeft;
+            title1.Name = "Title1";
+            title1.Text = "Time";
+            this.chart2.Titles.Add(title1);
+            // 
+            // ChartScroll
+            // 
+            this.ChartScroll.Location = new System.Drawing.Point(1007, 737);
+            this.ChartScroll.MaximumSize = new System.Drawing.Size(1198, 43);
+            this.ChartScroll.Name = "ChartScroll";
+            this.ChartScroll.Size = new System.Drawing.Size(1198, 43);
+            this.ChartScroll.TabIndex = 27;
+            this.ChartScroll.Scroll += new System.Windows.Forms.ScrollEventHandler(this.ChartScroll_Scroll);
+            this.ChartScroll.ValueChanged += new System.EventHandler(this.ChartScroll_ValueChanged);
+            // 
+            // ExportToPDF
+            // 
+            this.ExportToPDF.Location = new System.Drawing.Point(1910, 19);
+            this.ExportToPDF.Name = "ExportToPDF";
+            this.ExportToPDF.Size = new System.Drawing.Size(267, 61);
+            this.ExportToPDF.TabIndex = 28;
+            this.ExportToPDF.Text = "Eksportuj do PDF";
+            this.ExportToPDF.UseVisualStyleBackColor = true;
+            this.ExportToPDF.Click += new System.EventHandler(this.ExportToPDF_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(2227, 1287);
+            this.Controls.Add(this.ExportToPDF);
+            this.Controls.Add(this.ChartScroll);
             this.Controls.Add(this.chart2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.PlotX);
+            this.Controls.Add(this.PlotY);
+            this.Controls.Add(this.ChanegRangeY);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxMaxY);
+            this.Controls.Add(this.textBoxMinY);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.tabPage1);
             this.Controls.Add(this.GenerateChart);
-            this.Controls.Add(this.ChanegRange);
+            this.Controls.Add(this.ChanegRangeX);
             this.Controls.Add(this.OpenFile);
-            this.Controls.Add(this.textBoxMax);
+            this.Controls.Add(this.textMax);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBoxMin);
+            this.Controls.Add(this.textBoxMaxX);
+            this.Controls.Add(this.textBoxMinX);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
@@ -206,16 +333,27 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBoxMin;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBoxMinX;
+        private System.Windows.Forms.TextBox textBoxMaxX;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label textBoxMax;
+        private System.Windows.Forms.Label textMax;
         private System.Windows.Forms.Button OpenFile;
-        private System.Windows.Forms.Button ChanegRange;
+        private System.Windows.Forms.Button ChanegRangeX;
         private System.Windows.Forms.Button GenerateChart;
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
         private System.Windows.Forms.Panel tabPage1;
+        private System.Windows.Forms.Button ChanegRangeY;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxMaxY;
+        private System.Windows.Forms.TextBox textBoxMinY;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button PlotY;
+        private System.Windows.Forms.Button PlotX;
+        private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.HScrollBar ChartScroll;
+        private System.Windows.Forms.Button ExportToPDF;
     }
 }
 
--- KartaAC/Form1.cs
+++ KartaAC/Form1.cs
@@ -10,16 +10,20 @@ using System.Windows.Forms;
 using System.IO;
 using System.Windows.Forms.DataVisualization.Charting;
 
+
 namespace KartaAC
 {
     public partial class Form1 : Form
     {
         CsvLoading csvLoading = null;
+        string pathFiles = string.Empty;
+        List<string> listFileds = null;
+
         public Form1()
         {
             
             InitializeComponent();      
-            csvLoading = new CsvLoading(@"C:/Users/Laptop/Downloads/dane1.csv");
+           csvLoading = new CsvLoading(@"D:/danectp/dane1.csv");
         }
 
         DataGridView data = new DataGridView();
@@ -29,6 +33,36 @@ namespace KartaAC
         private void OpenFile_Click(object sender, EventArgs e)
         {
 
+            FolderBrowserDialog diag = new FolderBrowserDialog();
+            if (diag.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                textBox1.Text = diag.SelectedPath;
+                pathFiles = diag.SelectedPath;
+                string sourceEnvoa = diag.SelectedPath;
+                DirectoryInfo fieldE = new DirectoryInfo(sourceEnvoa);
+                listFileds = new List<string>();
+                var fieldsE = fieldE.GetFiles("*.*");
+                foreach (var field in fieldsE)
+                {
+                    listFileds.Add(field.Name);
+
+                }
+                comboBox1.DataSource = listFileds;
+            }
+            else
+            {
+                MessageBox.Show("You didn't selecet the folder!");
+            }
+            csvLoading = new CsvLoading(pathFiles + @"\" + listFileds.First());
+            LoadingData();
+
+
+        }
+
+        private void LoadingData()
+        {
+            
+
             data.Location = new Point(0, 0);
             data.Size = new Size(900, 962);
             data.BackgroundColor = Color.White;
@@ -37,39 +71,23 @@ namespace KartaAC
 
             data.DataSource = csvLoading.listData;
             tabPage1.Controls.Add(data);
-
-
         }
-        private void ChanegRange_Click(object sender, EventArgs e)
+        private void ChanegRangeX_Click(object sender, EventArgs e)
         {
-            double max = Double.Parse(textBoxMax.Text);
-            double min = Double.Parse(textBoxMin.Text);
+            
+            double maxX = double.Parse(textBoxMaxX.Text.Replace('.', ','));
+            double minX = double.Parse(textBoxMinX.Text.Replace('.', ','));
 
-            chart2.ChartAreas[0].AxisY.Maximum = max;
-            chart2.ChartAreas[0].AxisY.Minimum = min;
-
-
-            //double x;
-
-            //if (Double.TryParse(this.textBoxMax.Text, out x))
-            //{        
-            //    chart2.ChartAreas[0].AxisX.Maximum = Convert.ToDouble(this.textBoxMax.Text);
-
-            //}
-
-            //double y;
-
-            //if (Double.TryParse(this.textBoxMax.Text, out y) && y > chart2.ChartAreas[0].AxisX.Minimum)
-            //{
-            //    chart2.ChartAreas[0].AxisX.Minimum = Convert.ToDouble(this.textBoxMin.Text);
-
-            //}
+            chart2.ChartAreas[0].AxisX.Maximum = maxX;
+            chart2.ChartAreas[0].AxisX.Minimum = minX;
 
 
         }
 
         private void GenerateChart_Click(object sender, EventArgs e)
         {
+            table.Columns.Clear();
+            
             //Adding file cvs to datatable 
             table.Columns.Add("Time");
             table.Columns.Add("X");
@@ -84,24 +102,98 @@ namespace KartaAC
                 table.Rows.Add(row);
 
             }
-            
             chart2.DataSource = table;
+
+            chart2.Series.Clear();
+
+            chart2.Series.Add("X");
+            chart2.Series[0].ChartType = SeriesChartType.Line;
+            chart2.Series[0].Color = Color.Blue;
+
+            chart2.Series.Add("Y");
+            chart2.Series[1].ChartType = SeriesChartType.Line;
+            chart2.Series[1].Color = Color.Orange;
 
             chart2.Series["X"].Points.DataBindXY(table.DefaultView, "Time", table.DefaultView, "X");
             chart2.Series["Y"].Points.DataBindXY(table.DefaultView, "Time", table.DefaultView, "Y");
-
+            
+            
             chart2.DataBind();
-
+          
         }
 
-        private void chart1_Click(object sender, EventArgs e)
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
         }
 
-        private void panel1_Paint(object sender, PaintEventArgs e)
+
+        private void ChanegRangeY_Click(object sender, EventArgs e)
+        {
+
+            double maxY = double.Parse(textBoxMaxY.Text.Replace('.', ','));
+            double minY = double.Parse(textBoxMinY.Text.Replace('.', ','));
+
+            chart2.ChartAreas[0].AxisY.Maximum = maxY;
+            chart2.ChartAreas[0].AxisY.Minimum = minY;
+
+        }
+
+        private void PlotY_Click(object sender, EventArgs e)
+        {
+            chart2.Series.Clear();
+            chart2.Series.Add("OnlyY");
+            chart2.Series[0].ChartType = SeriesChartType.Line;
+            chart2.Series[0].Color = Color.Green;
+            chart2.Series["OnlyY"].Points.DataBindXY(table.DefaultView, "Time", table.DefaultView, "Y");
+
+        }
+
+        private void PlotX_Click(object sender, EventArgs e)
+        {
+            chart2.Series.Clear();
+            chart2.Series.Add("OnlyX");
+            chart2.Series[0].ChartType = SeriesChartType.Line;
+            chart2.Series[0].Color = Color.Red;
+            chart2.Series["OnlyX"].Points.DataBindXY(table.DefaultView, "Time", table.DefaultView, "X");
+        }
+
+        private void comboBox1_SelectedValueChanged(object sender, EventArgs e)
+        {
+            csvLoading = new CsvLoading(pathFiles + @"\" + comboBox1.SelectedValue);
+            LoadingData();
+        }
+
+        private void ChartScroll_Scroll(object sender, ScrollEventArgs e)
         {
 
         }
+
+        private void ExportToPDF_Click(object sender, EventArgs e)
+        {
+            
+            //string fileName = "Output.pdf";
+            //string filePath = "C://Users//Laptop//Desktop//Dane";
+            //if (!Directory.Exists(filePath))
+            //{
+            //    Directory.CreateDirectory(filePath);
+            //}
+            
+
+        }
+
+ 
+        private void ChartScroll_ValueChanged(object sender, EventArgs e)
+        {
+            List<double> a = csvLoading.listData.Select(x => double.Parse(x.Time)).ToList();
+            //List<double> b = csvLoading.listData.Select(x => double.Parse(x.Y)).ToList();
+            //MessageBox.Show(((int)a.Max()).ToString());
+            ChartScroll.Maximum = (int)a.Max();
+            chart2.ChartAreas[0].AxisX.Maximum = ChartScroll.Value + 20;
+            chart2.ChartAreas[0].AxisX.Minimum = ChartScroll.Value - 20;
+        }
+
+
     }
 }
