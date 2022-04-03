bb6d0efa9a02b0baaed7b52f3f1d8e10079ea8d6
Gaba54 <72696284+Gaba54@users.noreply.github.com>
2022-04-03 12:25:30 +0200

    CSVReader

---
+++ KartaAC/CsvLoading.cs
@@ -0,0 +1,48 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Globalization;
+using CsvHelper.Configuration;
+using CsvHelper.Configuration.Attributes;
+
+namespace KartaAC
+{
+    public class CsvLoading
+    {
+        string path = string.Empty;
+        public List<RowData> listData = null;
+    
+        public CsvLoading(string path)
+        {
+            this.path = path;
+            listData = new List<RowData>();
+            LoadingData();
+        }
+
+        private void LoadingData()
+        {
+            using (var streamReader = new StreamReader(path))
+            {
+                var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+                {
+                    Delimiter = "\t",
+                };
+
+                using (var csvReader = new CsvReader(streamReader, config))
+                {
+
+                    listData = csvReader.GetRecords<RowData>().ToList();
+                    
+                }
+            }
+        }
+       
+
+
+
+    }
+}
---
+++ KartaAC/RowData.cs
@@ -0,0 +1,23 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Globalization;
+using CsvHelper.Configuration;
+using CsvHelper.Configuration.Attributes;
+
+namespace KartaAC
+{
+    public class RowData
+    {
+        [Name("T [s]")]
+        public string Time { get; set; }
+        [Name("Ux [V]")]
+        public string X { get; set; }
+        [Name("Uy [V]")]
+        public string Y { get; set; }
+    }
+}
